68264be40665d13cf0cf9588e8161157e6b3ffc8
Sam Deane <sam@elegantchaos.com>
2018-07-12 12:27:38 +0100

    Detect if imwheel isn't installed, or isn't running.

--- interactive.sh
+++ interactive.sh
@@ -1,6 +1,18 @@
 # run imwheel
 
-if [[ $(grep IMWHEEL_START=1 /etc/X11/imwheel/startup.conf) == "" ]]
+if [[ $(which imwheel) = "" ]]
 then
-    echo "Set IMWHEEL_START=1 in /etc/X11/imwheel/startup.conf to enable mouse support."
+    echo "Mouse support requires imwheel. Use `sudo apt install imwheel` to install it."
+fi
+
+PROCS="$(ps ax)"
+if [[ $(ps ax) != *imwheel* ]]
+then
+    echo "imwheel wasn't running - starting it locally"
+    imwheel
+
+    if [[ $(grep IMWHEEL_START=1 /etc/X11/imwheel/startup.conf) == "" ]]
+    then
+        echo "Set IMWHEEL_START=1 in /etc/X11/imwheel/startup.conf to enable mouse support at startup."
+    fi
 fi
